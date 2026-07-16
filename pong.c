a494e2685fe1f544a1f583c5b4a91df63f0b898f
asmagaa <geraldciekniedripem@gmail.com>
2026-07-16 17:27:44 +0200

    add window and pong ball

--- pong.c
+++ pong.c
@@ -1,6 +1,39 @@
-#include <stdio.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_rect.h>
+#include <SDL2/SDL_surface.h>
+#include <SDL2/SDL_timer.h>
+#include <SDL2/SDL_video.h>
 
-int main(void) {
-    printf("Hello, World!");
+int main() {
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window *window = SDL_CreateWindow("Pong",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        640, 480, 0);
+    
+    SDL_Surface *surface = SDL_GetWindowSurface(window);
+
+    SDL_Rect rectangle = (SDL_Rect) { 
+        20, // x
+        20, // y
+        20, // width
+        20  // height
+    };
+
+    Uint32 color = 0xffffffff;
+
+    SDL_FillRect(surface, &rectangle, color);
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+
+    SDL_Delay(5000);
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
